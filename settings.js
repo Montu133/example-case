3f1cb5982700b5f99124bc8fcca863e3e2471719
Montu133 <tutnawa12@gmail.com>
2025-01-05 09:09:06 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,16 +10,16 @@ global.APIKeys = {
 	'https://api.zeeoneofc.my.id': 'Your Key',
 }
 
-global.namabot = "Bot Store"
-global.namaowner = "Zeeoneofc"
-global.packname = namabot
-global.author = namaowner
+global.namabot = "Bot montu"
+global.namaowner = "montu"
+global.packname = Montu13
+global.author = montu
 global.myweb = "https://s.id/zeeoneofc"
-global.footer_text = "© " + namabot
+global.footer_text = "© " + montu13
 global.pp_bot = fs.readFileSync("./image/foto.jpg")
 global.qris = fs.readFileSync("./image/qris.jpg")
-global.owner = ['62887435047326']
-global.pairingNumber = "" //pairing number to install bot 
+global.owner = ['6287836518013']
+global.pairingNumber = "6287836518013" //pairing number to install bot 
 global.sessionName = 'session'
 global.background = "https://telegra.ph/file/40342e0085d521cb8b907.jpg" //maks size 50kb agar welcome nya gk lemot
 global.prefa = ['-_-']
